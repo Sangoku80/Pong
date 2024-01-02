80b4faaa684c8b70a96d73def0fef13c68e28855
Sacha <sacha.droisy@gmail.com>
2024-01-02 20:53:04 +0100

    manque le game over et le nombre de points ainsi qu'un bouton pour rejouer.

--- core/src/com/mygdx/game/Ennemy.java
+++ core/src/com/mygdx/game/Ennemy.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Ennemy extends Paddle{
@@ -11,5 +13,22 @@ public class Ennemy extends Paddle{
     @Override
     public void Moves(float deltaTime) {
 
+        // inputs
+        if(Gdx.input.isKeyPressed(Input.Keys.UP))
+        {
+            if((this.y + this.height) < Gdx.graphics.getHeight())
+            {
+                this.y += (int) (deltaTime*speed);
+
+            }
+        }
+
+        if(y > 0)
+        {
+            if(Gdx.input.isKeyPressed((Input.Keys.DOWN)))
+            {
+                this.y -= (int) (deltaTime*speed);
+            }
+        }
     }
 }
