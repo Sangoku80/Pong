3919a74c7ed8512b2a9a0ef90fc9bf3390854bd5
Sacha <sacha.droisy@gmail.com>
2024-01-03 12:39:32 +0100

    Fenêtre n'est plus redimensionnable

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Pong");
 		config.setWindowedMode(800, 480);
+		config.setResizable(false);
 		config.useVsync(false);
 		config.setForegroundFPS(60);
 		new Lwjgl3Application(new Main(), config);
